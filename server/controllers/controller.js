4efcb1cbaf2c7caaf630dc645a87d0cbdf5dd6bb
Brian McKelvey <theturtle32@gmail.com>
2016-06-30 22:24:42 -0700

    Stub API returning mock data.

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,1 +1,71 @@
 'use strict'
+var casual = require('random-fixture-data');
+
+module.exports = {
+  getSquads: (req, res, next) => {
+    res.json([
+      generateSquad(),
+      generateSquad(),
+      generateSquad(),
+      generateSquad(),
+      generateSquad(),
+      generateSquad(),
+      generateSquad(),
+      generateSquad(),
+      generateSquad()
+    ]);
+  },
+  
+  getSquad: (req, res, next) => {
+    res.json(generateSquad());
+  },
+  
+  getEngineer: (req, res, next) => {
+    res.json(generateEngineer())
+  },
+  
+  getEngineers: (req, res, next) => {
+    res.json([
+      generateEngineer(),
+      generateEngineer(),
+      generateEngineer(),
+      generateEngineer(),
+      generateEngineer()
+    ]);
+  },
+  
+  createEngineer: (req, res, next) => {
+    var newEngineer = {
+      name: "Brian McKelvey",
+      username: "theturtle32",
+      squads: [
+        {
+          name: "Core Services",
+          current: true
+        },
+        {
+          name: "Renegades",
+          current: false
+        }
+      ]
+    }
+    res.json(newEngineer);
+  }
+};
+
+function generateEngineer() {
+  return {
+    name: casual.name,
+    username: casual.username,
+    squads: [
+      generateSquad({ current: true }),
+      generateSquad({ current: false }),
+      generateSquad({ current: false }),
+      generateSquad({ current: false })
+    ]
+  };
+}
+
+function generateSquad(augment={}) {
+  return Object.assign({ name: casual.title }, augment);
+}
--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,11 @@
 'use strict'
-const controller = require('./controller/controller.js');
-
+const controller = require('./controllers/controller.js');
 
 module.exports = (app, express) => {
   app.get('/');
-}
+  app.get('/squads', controller.getSquads);
+  app.get('/squads/:name', controller.getSquad);
+  app.get('/engineers', controller.getEngineers);
+  app.get('/engineers/:name', controller.getEngineer);
+  app.post('/engineers', controller.createEngineer);
+};
