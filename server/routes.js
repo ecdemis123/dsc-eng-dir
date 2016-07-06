1d80f34f95da85ca0c9949aa3930403e028abd27
Brian McKelvey <theturtle32@gmail.com>
2016-07-06 10:10:21 -0700

    Adding front-end

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,17 @@
 'use strict'
 const controller = require('./controllers/controller.js');
 const jsonParser = require('body-parser').json();
+const express = require('express');
 
 module.exports = (app, express) => {
+  app.use((req,res,next) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    if (req.get('Access-Control-Request-Headers')) {
+      res.set('Access-Control-Allow-Headers', req.get('Access-Control-Request-Headers'));
+    }
+    next();
+  });
+  app.use(express.static('public'));
   app.get('/');
   app.get('/squads', controller.getSquads);
   app.get('/squads/:name', controller.getSquad);
